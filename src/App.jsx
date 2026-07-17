483d7bf741c1fa4dfedc20ca0798d0619b516127
Alex Chen <achen2291@gmail.com>
2026-07-16 22:04:19 -0700

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
-function App() {
+import Register from "./auth/Register.jsx";
+import Login from "./auth/Login.jsx";
+import BooksPage from "./books/BooksPage.jsx";
+import Error404 from "./Error404.jsx";
+import Layout from "./layout/Layout.jsx";
+import Book from "./books/Book.jsx";
+
+import { Route, Routes } from "react-router";
+
+export default function Appp() {
   return (
-    <div>
-      <h1>
-        <img id="logo-image" src="books.png" />
-        Library App
-      </h1>
-
-      <p>
-        Complete the React components needed to allow users to browse a library catalog, check out
-        books, log in to their account,
-        <br />
-        log out of their account, review their account, and return books that they've finished
-        reading.
-      </p>
-
-      <p>
-        Recall that React really only cares about state. When our state changes our view changes as
-        well.
-      </p>
-
-      <p>
-        To that end, you may need to use some state in this top-level component in other components
-        by passing them down as a prop.
-      </p>
-      <p>OR</p>
-      <p>
-        Implement context in a way that allows all the components the need to know what's in the
-        context to have access to and be able to consume the right context.
-      </p>
-
-      <p>
-        Don't forget that our URL should dictate our view! Set up React Router to navigate between
-        the different views of your single page application!
-      </p>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<BooksPage />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
